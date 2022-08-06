9737a5a15eca436cfebfe693cee21911368d8dd1
Swapnil <65143129+swapnilAndhale@users.noreply.github.com>
2022-08-06 22:32:07 +0530

    Instafilter project added

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Swapnil on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
