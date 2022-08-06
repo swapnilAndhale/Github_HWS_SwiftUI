5f5ed20a1dea367d90f545191f78ec4d76d36be0
Swapnil <65143129+swapnilAndhale@users.noreply.github.com>
2022-08-06 22:23:18 +0530

    Animation file added

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Swapnil on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
