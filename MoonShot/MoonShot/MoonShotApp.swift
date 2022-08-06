50560cc779724e42289234c7fb68959ac09b3614
Swapnil <65143129+swapnilAndhale@users.noreply.github.com>
2022-08-06 22:43:10 +0530

    Swift files added to moonshot

---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Swapnil on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
