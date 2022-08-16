561332f0f6bccff4be557d12e734cc52c6cd0109
Swapnil <swapnilandhale3@gmail.com>
2022-08-16 15:58:44 +0530

    UI Updates

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
     
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -28,10 +31,40 @@ struct ContentView: View {
                 }
                 .onTapGesture {
                      //select an image
+                    showingImagePicker = true
+                }
+                
+                HStack {
+                    Text("Internsity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        //Change filter
+                    }
+                    Spacer()
+                    
+                    Button("Save", action: save)
                 }
                 
             }
+            .padding([.horizontal,.bottom])
+            .navigationTitle("Instafilter")
+            .onChange(of: inputImage) { _ in loadImage() }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+    func save() {
+        
     }
 }
 
