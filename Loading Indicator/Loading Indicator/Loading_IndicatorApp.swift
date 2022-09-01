b005bc7994a26e7d70a9b94e3a6421e03c7a4d02
Swapnil <swapnilandhale3@gmail.com>
2022-09-01 14:51:00 +0530

    Dot animation

---
+++ Loading Indicator/Loading Indicator/Loading_IndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loading_IndicatorApp.swift
+//  Loading Indicator
+//
+//  Created by Swapnil on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Loading_IndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
