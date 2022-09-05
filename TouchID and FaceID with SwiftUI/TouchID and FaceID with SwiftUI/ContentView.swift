bce5358bdc6c4b124f8dbbffdbb784b74916c868
Swapnil <swapnilandhale3@gmail.com>
2022-09-05 11:45:14 +0530

    TouchID and FaceID with SwiftUI project added

---
+++ TouchID and FaceID with SwiftUI/TouchID and FaceID with SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TouchID and FaceID with SwiftUI
+//
+//  Created by Swapnil on 05/09/22.
+//
+
+import LocalAuthentication
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchID and FaceID with SwiftUI/TouchID and FaceID with SwiftUI/TouchID_and_FaceID_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchID_and_FaceID_with_SwiftUIApp.swift
+//  TouchID and FaceID with SwiftUI
+//
+//  Created by Swapnil on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchID_and_FaceID_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
