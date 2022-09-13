059c518e6b12e4ac255575f89ab1002d84a739b8
Swapnil <swapnilandhale3@gmail.com>
2022-09-13 19:18:59 +0530

    TabView and QR code scanner added

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Swapnil on 12/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var prospects = Prospects()
+    
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontaced", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
+        .environmentObject(prospects)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Swapnil on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
