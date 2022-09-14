cdc331390596036004c36cb4e40e49bdec481ac7
Swapnil <swapnilandhale3@gmail.com>
2022-09-14 13:12:21 +0530

    Gesture project added

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Swapnil on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
