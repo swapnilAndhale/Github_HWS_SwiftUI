0326dc22640b7817ad01c2d12d1a755031982ce8
Swapnil <swapnilandhale3@gmail.com>
2022-09-17 20:02:07 +0530

    project added

---
+++ SnowSeeke/SnowSeeke/Resort.swift
@@ -0,0 +1,23 @@
+//
+//  Resort.swift
+//  SnowSeeke
+//
+//  Created by Swapnil on 17/09/22.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+}
+
---
+++ SnowSeeke/SnowSeeke/SnowSeekeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekeApp.swift
+//  SnowSeeke
+//
+//  Created by Swapnil on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
